c5663c80318fc4cf95c96179783fd295b82fb810
ginalyncox <gina.lyn.cox@gmail.com>
2023-09-30 20:30:57 -0500

    Updates 09/30/23

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const mongoose = require('mongoose');
 // Initialize Express App
 const app = express();
 
+// Middleware for body parsing
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 // Connect to MongoDB
 mongoose.connect('mongodb://localhost:27017/myBlog', {
   useNewUrlParser: true,
@@ -12,10 +16,11 @@ mongoose.connect('mongodb://localhost:27017/myBlog', {
 .then(() => console.log('MongoDB Connected'))
 .catch(err => console.log(err));
 
+// Include Routes
+const blogRoutes = require('./routes/blogRoutes');
+app.use('/api/blog', blogRoutes);
+
 // Start the server on port 3000
 app.listen(3000, () => {
   console.log('Server running on http://localhost:3000');
 });
-
-const blogRoutes = require('./blogRoutes');
-app.use('/api/blog', blogRoutes);
